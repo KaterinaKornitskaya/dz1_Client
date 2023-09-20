914c0ccfcb846dedfd12bd8bb9c2698e89632365
Радченко Катерина <ketrinradchenko@gmail.com>
2023-09-20 16:53:09 +0300

    Add project files.

---
+++ dz1_Client/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace dz1_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSendMessage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIpAddr = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonConnectServer = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSendMessage
+            // 
+            this.buttonSendMessage.Location = new System.Drawing.Point(21, 190);
+            this.buttonSendMessage.Name = "buttonSendMessage";
+            this.buttonSendMessage.Size = new System.Drawing.Size(161, 30);
+            this.buttonSendMessage.TabIndex = 10;
+            this.buttonSendMessage.Text = "Отправить сообщение";
+            this.buttonSendMessage.UseVisualStyleBackColor = true;
+            this.buttonSendMessage.Click += new System.EventHandler(this.buttonSendMessage_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(206, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "IP-адрес";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxIpAddr
+            // 
+            this.textBoxIpAddr.Location = new System.Drawing.Point(205, 41);
+            this.textBoxIpAddr.Name = "textBoxIpAddr";
+            this.textBoxIpAddr.Size = new System.Drawing.Size(111, 20);
+            this.textBoxIpAddr.TabIndex = 8;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(21, 101);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(226, 77);
+            this.textBoxMessage.TabIndex = 7;
+            // 
+            // buttonConnectServer
+            // 
+            this.buttonConnectServer.Location = new System.Drawing.Point(21, 18);
+            this.buttonConnectServer.Name = "buttonConnectServer";
+            this.buttonConnectServer.Size = new System.Drawing.Size(161, 43);
+            this.buttonConnectServer.TabIndex = 6;
+            this.buttonConnectServer.Text = "Соединение с сервером";
+            this.buttonConnectServer.UseVisualStyleBackColor = true;
+            this.buttonConnectServer.Click += new System.EventHandler(this.buttonConnectServer_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(272, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(226, 82);
+            this.listBox1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Запрос серверу";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Ответ от сервера";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 245);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonSendMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIpAddr);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonConnectServer);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSendMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIpAddr;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonConnectServer;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ dz1_Client/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.InteropServices;
+
+namespace dz1_Client
+{
+    public partial class Form1 : Form
+    {
+        public SynchronizationContext uiContext;
+        Socket socket;
+        public Form1()
+        {
+            InitializeComponent();
+            uiContext = SynchronizationContext.Current;
+        }
+        
+        // метод для соединения с сервером
+        public void ConnectWithServer()
+        {
+            try
+            {
+                // IP-адрес сервера - здесь адрес текущего компьютера (127.0.0.1)
+                IPAddress address = IPAddress.Loopback;
+                // либо IP-адрес, взятый их текстБокса
+                //textBoxMessage.Text = address.ToString();
+                // конечная точка - IP-адрес сервера + порт на сервере
+                IPEndPoint endPoint = new IPEndPoint(address, 49153);
+                // создаем потоковый сокет
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // соединяем сокет с конечной точкой ( с сервером)
+                socket.Connect(endPoint);
+
+                // берем имя узла компьютера клиаента (Dns.GetHostName()), конвертируем
+                // его в массив байт для передачи серверу
+                byte[] message = Encoding.Default.GetBytes(Dns.GetHostName());
+                // отпряавляем серверу сообщение
+                int byteSent = socket.Send(message);
+                // вывод информации о соединение на экран
+                MessageBox.Show($"Клиент {Dns.GetHostName()} установил " +
+                    $"соединение с сервером {socket.RemoteEndPoint.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // метод для отправки сообщения серверу
+        public void SendMessage()
+        {
+            try
+            {
+                // строка сообщения - из текстБокса
+                string message = textBoxMessage.Text;
+                // коневртируем строку сообщения в массив байт
+                byte[] msg = Encoding.Default.GetBytes(message);
+                // тправляем серверу сообщение через сокет с помощью метода Send()
+                int byteSent = socket.Send(msg);
+                // вызываем метод для получения ответа от сервера
+                ReceiveMas(socket);
+
+                // если наше сообщение (клиента) содержит "<end>
+                if (message.IndexOf("<end>") > -1)
+                {
+                    // 1024 - объем сообщения, буфер, и мы будем пытаться считать данные в этот буфер.
+                    // Но сколько именно вернется данных - вернется в int bytesRec с помощью Receive
+                    byte[] bytes = new byte[1024];
+                    // принимаем данные, переданные сервером. 
+                    int bytesRec = socket.Receive(bytes);
+                    // вывод сообщения 
+                    MessageBox.Show("Сервер (" + socket.RemoteEndPoint.ToString() + ") ответил: " + 
+                        Encoding.Default.GetString(bytes, 0, bytesRec));
+                    // блокируем передачу и получение данных для объекта Socket.
+                    socket.Shutdown(SocketShutdown.Both);
+                    // закрываем сокет
+                    socket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Клиент: " + ex.Message);
+            }
+        }
+        
+        // метод для получение сообщений от сервера
+        public void ReceiveMas(object param)
+        {
+            Socket handler = (Socket)param;
+            try
+            {
+                // создаем буфер
+                byte[] bytes = new byte[1024];
+                //int bytesRec = handler.Receive(bytes);
+
+                while (true)
+                {
+                    // получаем сообщение т сервера
+                    int bytesRec = handler.Receive(bytes);
+                    if (bytesRec == 0)  // если данных нет
+                    {
+                        // блокируем передачу данных
+                        handler.Shutdown(SocketShutdown.Both);
+                        // закрываем сокет
+                        handler.Close();
+                        return;
+                    }
+                    // полученное сообщение записываем в строку
+                    string data = Encoding.Default.GetString(bytes, 0, bytesRec);
+                    
+                    // выводим сообщение в элемент ЛистБокс с пом контекста синхронизации
+                    uiContext.Send(d => listBox1.Items.Add(data), null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Клиент: " + ex.Message);
+                handler.Shutdown(SocketShutdown.Both); // Блокируем передачу и получение данных для объекта Socket.
+                handler.Close(); // закрываем сокет
+            }
+        }
+
+        // обработка кнопки Соединение с сервером
+        private void buttonConnectServer_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(ConnectWithServer));
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        // обработка кнопки Отправить сообщение (серверу)
+        private void buttonSendMessage_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(SendMessage));
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        // обработка события Закрытие формы
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Клиент: " + ex.Message);
+            }
+        }
+    }
+}
